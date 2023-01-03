8847ad64de8f7b3ca570cd8a3631205dd85bdba0
Felipe Minetto <felipe@r2t.company>
2023-01-03 17:20:47 -0300

    List tasks on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import { GetServerSidePropsContext } from 'next'
+
+import { Task } from '../models'
 
 export default function Home() {
   return (
@@ -9,9 +12,24 @@ export default function Home() {
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: process.env.GRAPHQL_ENDPOINT
+  })
+  const {data:{listTasks}} = await client.query({
+    query:gql`query listTasks{
+      listTasks{
+          ID
+          title
+          completed
+          description
+          creation
+      }
+  }`,
+  })
   return {
     props: {
-
+      data:listTasks.map((element:any)=>new Task(element.ID, element.title, element.description, element.creation, element.completed))
     }
   }
 }
